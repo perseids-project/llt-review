1d5d37151055472f7242aed0cf74afdf17edfa82
LFDM <1986gh@gmail.com>
2014-03-17 11:46:18 +0100

    Add report method for sentence

--- lib/llt/diff/parser/sentence.rb
+++ lib/llt/diff/parser/sentence.rb
@@ -3,11 +3,33 @@ module LLT
     class Sentence
       def initialize(id)
         @id = id
-        @words = {}
+        @words  = {}
       end
 
       def add_word(id, word)
         @words[id] = word
+      end
+
+      def report
+        @report ||= create_report
+      end
+
+      private
+
+      def create_report
+        report_hash.each do |category, res|
+          @words.each do |id, word|
+            res[word.send(category)] += 1
+          end
+        end
+      end
+
+      def report_hash
+        %w{ relation }.each_with_object({}) { |e, hsh| hsh[e] = counter_hash }
+      end
+
+      def counter_hash
+        Hash.new(0)
       end
     end
   end
