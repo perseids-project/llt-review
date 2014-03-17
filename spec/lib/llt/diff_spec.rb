6e0e230c978464c3761b74182c5077e2550ffffd
LFDM <1986gh@gmail.com>
2014-03-17 23:33:26 +0100

    Refine spec

--- spec/lib/llt/diff_spec.rb
+++ spec/lib/llt/diff_spec.rb
@@ -9,29 +9,33 @@ describe LLT::Diff do
 
   let(:g1) do
     <<-EOF
-      <sentence id="21" document_id="Perseus:text:1999.02.0002" subdoc="Book=2:chapter=5" span="In3:erat0">
-        <word id="1" form="In" lemma="in1" postag="r--------" head="5" relation="AuxP"/>
-        <word id="2" form="eo" lemma="is1" postag="p-s---nb-" head="3" relation="ATR"/>
-        <word id="3" form="flumine" lemma="flumen1" postag="n-s---nb-" head="1" relation="ADV"/>
-        <word id="4" form="pons" lemma="pons1" postag="n-s---mn-" head="5" relation="SBJ"/>
-        <word id="5" form="erat" lemma="sum1" postag="v3siia---" head="0" relation="PRED"/>
-      </sentence>
+      <treebank>
+        <sentence id="21" document_id="Perseus:text:1999.02.0002" subdoc="Book=2:chapter=5" span="In3:erat0">
+          <word id="1" form="In" lemma="in1" postag="r--------" head="5" relation="AuxP"/>
+          <word id="2" form="eo" lemma="is1" postag="p-s---nb-" head="3" relation="ATR"/>
+          <word id="3" form="flumine" lemma="flumen1" postag="n-s---nb-" head="1" relation="ADV"/>
+          <word id="4" form="pons" lemma="pons1" postag="n-s---mn-" head="5" relation="SBJ"/>
+          <word id="5" form="erat" lemma="sum1" postag="v3siia---" head="0" relation="PRED"/>
+        </sentence>
+      </treebank>
     EOF
   end
 
   let(:r1) do
     <<-EOF
-      <sentence id="21" document_id="Perseus:text:1999.02.0002" subdoc="Book=2:chapter=5" span="In3:erat0">
-        <word id="1" form="In" lemma="in1" postag="r--------" head="4" relation="AuxP"/>
-        <word id="2" form="eo" lemma="is1" postag="p-s---nb-" head="3" relation="ATR"/>
-        <word id="3" form="flumine" lemma="flumen2" postag="n-s---nd-" head="1" relation="ADV"/>
-        <word id="4" form="pons" lemma="pons1" postag="n-s---mn-" head="5" relation="OBJ"/>
-        <word id="5" form="erat" lemma="sum1" postag="v3siia---" head="0" relation="PRED"/>
-      </sentence>
+      <treebank>
+        <sentence id="21" document_id="Perseus:text:1999.02.0002" subdoc="Book=2:chapter=5" span="In3:erat0">
+          <word id="1" form="In" lemma="in1" postag="r--------" head="4" relation="AuxP"/>
+          <word id="2" form="eo" lemma="is1" postag="p-s---nb-" head="3" relation="ATR"/>
+          <word id="3" form="flumine" lemma="flumen2" postag="n-s---nd-" head="1" relation="ADV"/>
+          <word id="4" form="pons" lemma="pons1" postag="n-s---mn-" head="5" relation="OBJ"/>
+          <word id="5" form="erat" lemma="sum1" postag="v3siia---" head="0" relation="PRED"/>
+        </sentence>
+      </treebank>
     EOF
   end
 
-  describe "#compare" do
+  describe "#diff" do
     it "creates a diff report of a gold and review annotation" do
       allow(differ).to receive(:get_from_uri).with(:uri_for_g1) { g1 }
       allow(differ).to receive(:get_from_uri).with(:uri_for_r1) { r1 }
@@ -40,6 +44,16 @@ describe LLT::Diff do
       result.should have(1).item         # we had one reviewable annotation
       result[0].should have(1).item      # one sentence with differences
       result[0][21].should have(3).items # and 3 words with differences
+    end
+
+    it "takes multiple gold and review files" do
+      allow(differ).to receive(:get_from_uri).with(:uri_for_g1) { g1 }
+      allow(differ).to receive(:get_from_uri).with(:uri_for_g2) { g1 }
+      allow(differ).to receive(:get_from_uri).with(:uri_for_r1) { r1 }
+      allow(differ).to receive(:get_from_uri).with(:uri_for_r2) { r1 }
+
+      result = differ.diff(%i{ uri_for_g1 uri_for_g2 }, %i{ uri_for_r1 uri_for_r2 })
+      result.should have(4).items # we have two times two reviewable annotations now
     end
   end
 end
