076391d8493c86e4846fd67538be2eed8113d75b
LFDM <1986gh@gmail.com>
2014-03-19 00:42:15 +0100

    Add #counts to Gold

--- lib/llt/diff/parser/gold.rb
+++ lib/llt/diff/parser/gold.rb
@@ -10,6 +10,14 @@ module LLT
       def initialize(id, sentences)
         super(id)
         @sentences = sentences
+        @counts    = {}
+      end
+
+      def counts
+        return @counts if @counts.any?
+        @counts[:sentences] = { total: @sentences.count }
+        full_report = merge_reports(*@sentences.map { |_, s| s.report })
+        @counts.merge!(sort_report(full_report))
       end
     end
   end
