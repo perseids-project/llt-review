be9339476d1907f656858b9cc96636562979f047
LFDM <1986gh@gmail.com>
2014-03-19 11:55:15 +0100

    Refactor of Diff for more flexibility
    The goal is to add Reports on their own.

--- lib/llt/diff.rb
+++ lib/llt/diff.rb
@@ -8,9 +8,18 @@ module LLT
     include Core::Api::Helpers
 
     def diff(gold, reviewables)
-      parse_files(gold, reviewables)
+      parses = parse_files(Gold: gold, Reviewable: reviewables)
+
+      @gold, @reviewables = parses.partition do |parse_data|
+        parse_data.instance_of?(Parser::Gold)
+      end
+
       compare
       all_diffs
+    end
+
+    def to_xml(type = :xml)
+      XML_DECLARATION + wrap_with_tag('doc', header + send("#{type}_to_xml"))
     end
 
     private
@@ -25,11 +34,9 @@ module LLT
       end
     end
 
-    def parse_files(gold, reviewables)
-      to_parse = gold.map { |uri| [:Gold, uri] } + reviewables.map { |uri| [:Reviewable, uri] }
-      @gold, @reviewables = parse_threaded(to_parse).partition do |parse_data|
-        parse_data.instance_of?(Parser::Gold)
-      end
+    def parse_files(files)
+      to_parse = files.flat_map { |klass, uris| uris.map { |uri| [klass, uri] } }
+      parse_threaded(to_parse)
     end
 
     def parse_threaded(uris_with_classes)
@@ -42,12 +49,12 @@ module LLT
       threads.map { |t| t.join; t.value }
     end
 
-    def to_xml
-      XML_DECLARATION + wrap_with_tag('doc', header + diff_to_xml)
+    def header
+      wrap_with_tag('files', header_files.map(&:xml_heading).join)
     end
 
-    def header
-      wrap_with_tag('files', (@gold + @reviewables).map(&:xml_heading).join)
+    def header_files
+      [@gold, @reviewables, @reports].flatten.compact
     end
 
     def wrap_with_tag(tag, content)
