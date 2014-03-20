9ea1237b5322d04f55c336e0e95986d37ac94ed3
LFDM <1986gh@gmail.com>
2014-03-20 19:44:20 +0100

    Enable multi-threading for jruby

--- lib/llt/diff/parser/report.rb
+++ lib/llt/diff/parser/report.rb
@@ -22,6 +22,28 @@ module LLT
         end
       end
 
+      def collect_singlethreaded
+        @sentences.map { |_, s| s.report }.each do |rep|
+          rep.each { |_, r| add(r) }
+        end
+      end
+
+      def collect_multithreaded
+        slice_size = @sentences.size / 4
+        threads = []
+        @sentences.each_slice(slice_size) do |slice|
+          temp_container = Report.new(nil, {})
+          threads << Thread.new do
+            slice.map { |_, s| s.report }.each do |rep|
+              rep.each { |_, r| temp_container.add(r) }
+            end
+            temp_container
+          end
+        end
+        temps = threads.flat_map { |t| t.join; t.value }
+        temps.each { |temp| temp.each { |_, r| add(r) }}
+      end
+
       def xml_attributes
         { id: @id }
       end
