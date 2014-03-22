c0434b086a2db33900d3fced940fb7e4127e8700
LFDM <1986gh@gmail.com>
2014-03-22 12:28:44 +0100

    Add comment in PostagDiff about its current status

--- lib/llt/diff/parser/postag_diff.rb
+++ lib/llt/diff/parser/postag_diff.rb
@@ -1,5 +1,7 @@
 module LLT
   class Diff::Parser
+    # This class is redundant right now, we keep it around because we
+    # still need to decide if we show datapoint diffs
     class PostagDiff
       attr_reader :original, :new
 
