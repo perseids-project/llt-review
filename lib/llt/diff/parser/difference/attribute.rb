2507e8d68813e1a7a7ff3eb001f8a426a3f333b3
LFDM <1986gh@gmail.com>
2014-03-23 12:11:26 +0100

    Work on reporting of differences of Attributes

--- lib/llt/diff/parser/difference/attribute.rb
+++ lib/llt/diff/parser/difference/attribute.rb
@@ -1,8 +1,14 @@
 module LLT
   module Diff::Parser::Difference
-    class Attribute
+    class Attribute < Generic
       def write_to_report(report, unique)
-        report[report_location][@attribute].add_wrong(unique)
+        container = report[report_location]
+        container.add_wrong(unique)
+        container[diff_location].add_wrong(unique)
+      end
+
+      def diff_location
+        @original
       end
     end
   end
--- lib/llt/diff/parser/difference/head.rb
+++ lib/llt/diff/parser/difference/head.rb
@@ -1,6 +1,10 @@
 module LLT
   module Diff::Parser::Difference
     class Head < Attribute
+      def write_to_report(report, unique)
+        report[report_location].add_wrong(unique)
+      end
+
       def report_location
         :heads
       end
