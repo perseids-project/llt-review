dfc6ca045539a21c97bf6bb26ca6baf0a057c433
LFDM <1986gh@gmail.com>
2014-03-23 11:48:14 +0100

    Totally disable word_diff_spec

--- spec/lib/llt/diff/parser/word_diff_spec.rb
+++ spec/lib/llt/diff/parser/word_diff_spec.rb
@@ -1,16 +1,16 @@
-require 'spec_helper'
+#require 'spec_helper'
 
-describe LLT::Diff::Parser::WordDiff do
-  let(:difference) { LLT::Diff::Parser::WordDiff.new(1) }
+#describe LLT::Diff::Parser::WordDiff do
+  #let(:difference) { LLT::Diff::Parser::WordDiff.new(1) }
 
-  describe "#to_xml" do
-    it "returns differences grouped by original and new" do
-      pending('Implementation changed')
-      difference.lemma  = ['a', 'b']
-      difference.postag = ['-a-', '-b-']
-      difference.head   = [19, 20]
-      res = '<word id="1"><original lemma="a" postag="-a-" head="19"/><new lemma="b" postag="-b-" head="20"/></word>'
-      difference.to_xml.should == res
-    end
-  end
-end
+  #describe "#to_xml" do
+    #it "returns differences grouped by original and new" do
+      #pending('Implementation changed')
+      #difference.lemma  = ['a', 'b']
+      #difference.postag = ['-a-', '-b-']
+      #difference.head   = [19, 20]
+      #res = '<word id="1"><original lemma="a" postag="-a-" head="19"/><new lemma="b" postag="-b-" head="20"/></word>'
+      #difference.to_xml.should == res
+    #end
+  #end
+#end
