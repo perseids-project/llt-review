6ec293d0e193e057ea503620dae7a96287a80e48
LFDM <1986gh@gmail.com>
2014-03-27 21:26:37 +0100

    Add Alignment::Parser::NokogiriHandler

--- lib/llt/diff/alignment/parser/nokogiri_handler.rb
+++ lib/llt/diff/alignment/parser/nokogiri_handler.rb
@@ -5,6 +5,7 @@ module LLT
     class NokogiriHandler < Nokogiri::XML::SAX::Document
 
       include Diff::Helpers::Parsing::Helper
+      include Diff::Helpers::Parsing::Helper::ForNokogiri
       include Helper
 
       def parse(data)
@@ -12,6 +13,30 @@ module LLT
       end
 
       def start_element(name, attrs = [])
+        case name
+        when 'w'        then register_word(first_val(attrs))
+        when 'text'     then @in_text = true
+        when 'refs'     then register_translation(first_val(attrs)) unless @original
+        when 'wds'      then set_orig_or_translation(first_val(attrs))
+        when 'sentence' then register_sentence(first_val(attrs))
+        when 'language' then set_languages(attrs)
+        end
+      end
+
+      def end_element(name)
+        @in_text = false if name == 'text'
+      end
+
+      def characters(string)
+        set_text(string)
+      end
+
+      private
+
+      def set_languages(attrs)
+        hsh = Hash[attrs]
+        lang = hsh['xml:lang']
+        hsh['lnum'] == 'l1' ? @lang1 = lang : @lang2 = lang
       end
     end
   end
