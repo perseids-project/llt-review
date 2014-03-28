127fece6017f497ffae65fef9b326df98172bb7f
LFDM <1986gh@gmail.com>
2014-03-28 11:59:17 +0100

    Move Treebank::Report code to Common

--- lib/llt/diff/treebank/gold.rb
+++ lib/llt/diff/treebank/gold.rb
@@ -1,14 +1,7 @@
 module LLT
   class Diff::Treebank
     class Gold < Report
-      attr_reader :sentences
-
-      # Check Comparison#report to learn more
-      def clone
-        cloned = super
-        cloned.replace_with_clone(:sentences, :report)
-        cloned
-      end
+      include Diff::Common::Golden
     end
   end
 end
--- lib/llt/diff/treebank/report.rb
+++ lib/llt/diff/treebank/report.rb
@@ -1,6 +1,6 @@
 module LLT
   class Diff::Treebank
-    class Report
+    class Report < Diff::Common::Report
       require 'llt/diff/treebank/report/generic'
       require 'llt/diff/treebank/report/datapoints'
       require 'llt/diff/treebank/report/sentences'
@@ -10,58 +10,10 @@ module LLT
       require 'llt/diff/treebank/report/postag/datapoint'
       require 'llt/diff/treebank/report/relation'
 
-      include Diff::Helpers::Reportable
-
-      attr_reader :sentences
-
-      def initialize(id, sentences)
-        super(id)
-        @sentences = sentences
-      end
-
-      def report
-        @report ||= begin
-          add_report_container
-          if RUBY_ENGINE == 'jruby'
-            collect_multithreaded
-          else
-            collect_singlethreaded
-          end
-          @container.each { |_, rep| rep.sort! }
-        end
-      end
-
-      def collect_singlethreaded
-        @sentences.map { |_, s| s.report }.each do |rep|
-          rep.each { |_, r| add(r) }
-        end
-      end
-
-      def collect_multithreaded
-        slice_size = @sentences.size / 4
-        slice_size = 1 if slice_size.zero?
-        threads = []
-        @sentences.each_slice(slice_size) do |slice|
-          temp_container = Report.new(nil, {})
-          threads << Thread.new do
-            slice.map { |_, s| s.report }.each do |rep|
-              rep.each { |_, r| temp_container.add(r) }
-            end
-            temp_container
-          end
-        end
-        temps = threads.flat_map { |t| t.join; t.value }
-        temps.each { |temp| temp.each { |_, r| add(r) }}
-      end
-
-      def xml_attributes
-        { id: @id }
-      end
-
       private
 
-      def add_report_container
-        add(Sentences.new(@sentences.count))
+      def namespace
+        Report
       end
     end
   end
