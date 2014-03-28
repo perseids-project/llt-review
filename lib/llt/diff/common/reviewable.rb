306c66d3595b4e69cbb9a67e02219f9cd542825e
LFDM <1986gh@gmail.com>
2014-03-28 12:07:26 +0100

    Move Reviewable code to Common

--- lib/llt/diff/common/reviewable.rb
+++ lib/llt/diff/common/reviewable.rb
@@ -1,6 +1,34 @@
 module LLT
   class Diff::Common
     class Reviewable
+      include Diff::Helpers::HashContainable
+
+      attr_reader :sentences
+
+      def initialize(id, sentences)
+        super(id)
+        @sentences = sentences
+      end
+
+      # can't use the class methods that set the same, as we have
+      # subclasses using this value as well
+      def xml_tag
+        :comparison
+      end
+
+      def diff
+        @container
+      end
+
+      def compare(gold)
+        comparison = Comparison.new(gold, self)
+        comparison.compare
+        add(comparison) if comparison.any?
+      end
+
+      def to_xml
+        container_to_xml
+      end
     end
   end
 end
--- lib/llt/diff/treebank/reviewable.rb
+++ lib/llt/diff/treebank/reviewable.rb
@@ -1,28 +1,6 @@
 module LLT
   class Diff::Treebank
-    class Reviewable
-      include Diff::Helpers::HashContainable
-
-      container_alias :diff
-
-      xml_tag :reviewable
-
-      attr_reader :sentences
-
-      def initialize(id, sentences)
-        super(id)
-        @sentences = sentences
-      end
-
-      def compare(gold)
-        comparison = Comparison.new(gold, self)
-        comparison.compare
-        add(comparison) if comparison.any?
-      end
-
-      def to_xml
-        container_to_xml
-      end
+    class Reviewable < Diff::Common::Reviewable
     end
   end
 end
