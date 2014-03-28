51e3894c024f655dfe5ff8561a751c595814b988
LFDM <1986gh@gmail.com>
2014-03-28 12:24:39 +0100

    Delegate compare from Treebank::Sentence to Word

--- lib/llt/diff/treebank/sentence.rb
+++ lib/llt/diff/treebank/sentence.rb
@@ -5,30 +5,13 @@ module LLT
 
       container_alias :words
 
-      def initialize(id)
-        super
-        # Eventually we'll need this configurable
-        @comparable_elements = %i{ lemma postag head relation }
-      end
-
       def report
         @report ||= create_report
       end
 
-      # Ideally we can pass word comparisons along to Word#compare
       def compare(other)
         diff = Difference::Sentence.new(self)
-        words.each do |id, word|
-          other_word = other[id]
-          @comparable_elements.each do |comparator|
-            a, b = [word, other_word].map { |w| w[comparator].to_s }
-            if a != b
-              d = diff[id] ||= Difference::Word.new(word)
-              d.add(new_difference(word, comparator, a, b))
-            end
-          end
-        end
-
+        words.each { |id, word| word.compare(other[id], diff) }
         diff
       end
 
@@ -59,11 +42,6 @@ module LLT
         reports.each_with_object({}) do |(tag, requested), hsh|
           hsh[tag] = Report::Generic.new(tag, size, requested)
         end.merge(postags: Report::Postags.new(size))
-      end
-
-      def new_difference(word, comparator, original, new)
-        klass = Difference.const_get(comparator.capitalize)
-        klass.new(word[comparator], original, new)
       end
     end
   end
