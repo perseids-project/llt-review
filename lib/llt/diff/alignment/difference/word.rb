0f7f8cd85f627dd4ee751a935eeb608d86cb6ee2
LFDM <1986gh@gmail.com>
2014-04-05 02:18:32 +0200

    Start work on Alignment diff reporting

--- lib/llt/diff/alignment/difference/word.rb
+++ lib/llt/diff/alignment/difference/word.rb
@@ -1,10 +1,9 @@
 module LLT
   module Diff::Alignment::Difference
-    class Word
-      include Diff::Helpers::HashContainable
-      include Diff::Helpers::DiffReporter
-
-      xml_tag :word
+    class Word < Diff::Common::Difference::Word
+      def diff_id
+        @diff_id ||= "#{@id}:#{item.text}|#{item.translation}"
+      end
     end
   end
 end
