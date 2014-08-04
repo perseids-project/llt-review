3b4a150c0ef24a4d3c0155220a7e239ac374cff2
LFDM <1986gh@gmail.com>
2014-08-05 00:47:54 +0200

    Add a route for html

--- lib/llt/review/api.rb
+++ lib/llt/review/api.rb
@@ -5,22 +5,21 @@ require 'llt/review'
 class Api < Sinatra::Base
   register Sinatra::RespondWith
 
+  set :root, File.expand_path('../../../..', __FILE__)
+
   get '/:type/diff' do
-    gold = Array(params[:gold])
-    rev  = Array(params[:reviewable])
-
-    comp_param = params[:compare]
-    comparables = comp_param ? Array(comp_param).map(&:to_sym) : nil
-
-    klass = params[:type]
-    # return an error if klass is neither treebank nor Alignment
-
-    diff = LLT::Review.const_get(klass.capitalize).new
-    puts comparables
-    diff.diff(gold, rev, comparables)
+    diff = process_diff(params)
 
     respond_to do |f|
       f.xml { diff.to_xml }
+    end
+  end
+
+  get '/:type/diff/:view' do
+    diff = process_diff(params)
+
+    if params[:view] == 'html'
+      haml :index, locals: { diff: diff }
     end
   end
 
@@ -33,5 +32,20 @@ class Api < Sinatra::Base
     respond_to do |f|
       f.xml { reporter.to_xml(:report)}
     end
+  end
+
+  def process_diff(params)
+    gold = Array(params[:gold])
+    rev  = Array(params[:reviewable])
+
+    comp_param = params[:compare]
+    comparables = comp_param ? Array(comp_param).map(&:to_sym) : nil
+
+    klass = params[:type]
+    # return an error if klass is neither treebank nor Alignment
+
+    diff = LLT::Review.const_get(klass.capitalize).new
+    diff.diff(gold, rev, comparables)
+    diff
   end
 end
