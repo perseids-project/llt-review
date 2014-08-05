cbc0b52e1eaf3495e5dd6aadefc996bb1e59f900
LFDM <1986gh@gmail.com>
2014-08-05 12:58:05 +0200

    Use the tooltipsy tooltip
    The pure css tooltip can't provide us html content

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,2 +1,11 @@
 $(document).ready(function() {
+  function addTokenTooltips() {
+    $('.token.tooltip').tooltipsy({
+      content: function($el) { return $el.attr('tooltip'); },
+      delay: 0
+    });
+  }
+
+
+  addTokenTooltips();
 });
