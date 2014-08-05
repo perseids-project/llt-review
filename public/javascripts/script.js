43a15081ee1aeb6488599cbbe41a6169dd7cd169
LFDM <1986gh@gmail.com>
2014-08-05 14:19:09 +0200

    Highlight heads

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,6 +4,14 @@ $(document).ready(function() {
       content: function($el) { return $el.attr('tooltip'); },
       delay: 0
     });
+
+    $('.token.head-error').each(function() {
+      var el = $(this);
+      el.hover(function() {
+        $('#' + el.attr('hr')).toggleClass('success-bg');
+        $('#' + el.attr('hw')).toggleClass('error-bg');
+      });
+    });
   }
 
 
