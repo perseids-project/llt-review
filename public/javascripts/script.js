b4d5b0622ad70fe350eefa7e78084db20c17f3c9
LFDM <1986gh@gmail.com>
2014-08-08 21:27:32 +0200

    Refactor of percent to color

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,39 +1,34 @@
 $(document).ready(function() {
 
   function hsv2rgb(h, s, v) {
+  }
+
+  function percentToColor(percent) {
+    var h = Math.floor((100 - percent) * 120/ 100);
+    var s = 1, v = 1;
+
     var rgb, i, data = [];
     h = h / 60;
     i = Math.floor(h);
     data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
     switch(i) {
       case 0:
-        rgb = [v, data[2], data[0]];
-        break;
+        rgb = [v, data[2], data[0]]; break;
       case 1:
-        rgb = [data[1], v, data[0]];
-        break;
+        rgb = [data[1], v, data[0]]; break;
       case 2:
-        rgb = [data[0], v, data[2]];
-        break;
+        rgb = [data[0], v, data[2]]; break;
       case 3:
-        rgb = [data[0], data[1], v];
-        break;
+        rgb = [data[0], data[1], v]; break;
       case 4:
-        rgb = [data[2], data[0], v];
-        break;
+        rgb = [data[2], data[0], v]; break;
       default:
-        rgb = [v, data[0], data[1]];
-        break;
+        rgb = [v, data[0], data[1]]; break;
     }
 
     return '#' + rgb.map(function(x){
       return ("0" + Math.round(x*255).toString(16)).slice(-2);
     }).join('');
-  }
-
-  function percentToColor(percent) {
-    var h = Math.floor((100 - percent) * 120/ 100);
-    return hsv2rgb(h, 1, 1);
   }
 
 
