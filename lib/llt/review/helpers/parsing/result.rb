db29040cd2f8275ad0d2462b226c08c0f6424dc4
LFDM <1986gh@gmail.com>
2014-08-11 10:22:13 +0200

    Parse annotator, format and lang in oxHandler

--- lib/llt/review/helpers/parsing/result.rb
+++ lib/llt/review/helpers/parsing/result.rb
@@ -1,6 +1,12 @@
 module LLT
   module Review::Helpers::Parsing
     class Result
+      attr_accessor :format, :lang
+
+      def annotators
+        @annotators ||= Helper::Annotators.new
+      end
+
       include Core::Structures::HashContainable
     end
   end
--- lib/llt/review/treebank/parser/ox_handler.rb
+++ lib/llt/review/treebank/parser/ox_handler.rb
@@ -15,6 +15,12 @@ module LLT
         case name
         when :word     then @in_word = true
         when :sentence then @in_sentence = true
+        when :annotator then @in_annotator = true
+        when :treebank then @in_treebank = true
+        end
+
+        if @in_annotator
+          instance_variable_set("@in_#{name}", true);
         end
       end
 
@@ -22,6 +28,22 @@ module LLT
         case name
         when :word     then @in_word = false
         when :sentence then @in_sentence = false
+        when :annotator then @in_annotator = false
+        end
+
+        if @in_annotator
+          instance_variable_set("@in_#{name}", false);
+        end
+      end
+
+      def annotator
+        @annotator ||= Annotator.new
+      end
+
+      def add_complete_annotator
+        if annotator.complete?
+          @result.annotators.add(annotator)
+          @annotator = nil
         end
       end
 
@@ -35,6 +57,21 @@ module LLT
           end
         when @in_sentence
           register_sentence(value) if name == :id
+        when @in_treebank
+          register_language(value) if name == :"xml:lang"
+          register_format(value)   if name == :format
+        end
+      end
+
+      def text(value)
+        if @in_annotator
+          params = [:short, :name, :address, :url]
+          params.each do |param|
+            if instance_variable_get("@in_#{param}")
+              annotator.send("#{param}=", value)
+              add_complete_annotator
+            end
+          end
         end
       end
     end
